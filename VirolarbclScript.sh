b350ba8873c0860cb4d7ede8ca873fe1e50db305
EmilioTrujillo03 <ejtrujillo@puce.edu.ec>
2025-07-15 15:05:43 -0500

    ScriptAñadido

---
+++ VirolarbclScript.sh
@@ -0,0 +1,13 @@
+# SCRIPT PARA DESCARGAR, ALINEAR Y EJECUTAR FILOGENIA DE VIROLA. POR FAVOR REVISAR README.md PARA COMPRENDER PASOS INTERMEDIOS NO ESPECÍFICADOS
+
+/u/scratch/d/dechavez/Bioinformatica-PUCE/MastBio/edirect/esearch -db nuccore -query "rbcl [GENE] AND Virola[ORGN]" | efetch -format fasta > rbcl-Virola.fasta
+
+/u/scratch/d/dechavez/Bioinformatica-PUCE/MastBio/edirect/esearch -db nuccore -query "rbcl [GENE] AND Osteophloeum[ORGN]" | efetch -format fasta > rbcl-Osteophloeum.fasta
+
+/u/scratch/d/dechavez/Bioinformatica-PUCE/MastBio/edirect/esearch -db nuccore -query "rbcl [GENE] AND Compsoneura[ORGN]" | efetch -format fasta > rbcl-Compsoneura.fasta
+
+cat rbcl-Compsoneura.fasta rbcl-Duguetia.fasta rbcl-Osteophloeum.fasta rbcl-Virola.fasta > MatrizMagnoliales.fasta
+
+./muscle3.8.31_i86linux64 -in MatrizMagnoliales.fasta -out MatrizMagnolialesALN.fasta
+
+iqtree -s MatrizMagnolialesALN.fasta -o AY738160.1_Duguetia_guianensis -m HKY+I+G4 -bb 1000 -minsup 0.5
